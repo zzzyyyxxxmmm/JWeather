2ff68e9f239816e8f92f71831e816f150a8e7d1e
zzzyyyxxxmmm <wjk32111@gmail.com>
2018-01-06 21:19:21 -0500

    Initial commit

---
+++ app/src/main/java/com/wjk32/jweather/WelcomeActivity.java
@@ -0,0 +1,43 @@
+package com.wjk32.jweather;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+
+import static java.lang.Thread.sleep;
+
+/**
+ * Created by wjk32 on 1/6/2018.
+ */
+
+public class WelcomeActivity extends Activity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Observable.just(initialdata())
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(result -> MainActivity());
+        }
+
+    private void MainActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+    public String initialdata(){
+        try{
+
+            Thread.sleep(3000);
+        }catch (Exception e){
+            System.out.println(e);
+        }
+        return "";
+    }
+}
